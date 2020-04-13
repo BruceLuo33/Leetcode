b7f954b32dc4f8a4486bf190f25a18339365691e
Yifan <yifanluo33@gmail.com>
2020-04-13 20:10:52 +0800

    4sum Completed

--- editor/cn/[18]四数之和.java
+++ editor/cn/[18]四数之和.java
@@ -25,77 +25,46 @@ import java.util.Arrays;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        if(nums==null||nums.length<4){
-            return result;
-        }
-        /*对数组进行从小到大排序*/
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums == null || nums.length < 4) return ans;
+        int len = nums.length;
         Arrays.sort(nums);
-        /*数组长度*/
-        int length=nums.length;
-        /*定义4个指针k，i，j，h  k从0开始遍历，i从k+1开始遍历，留下j和h，j指向i+1，h指向数组最大值*/
-        for(int k = 0; k < length-3; k++){
-            /*当k的值与前面的值相等时忽略*/
-            if(k > 0 && nums[k] == nums[k-1]){
-                continue;
-            }
-            /*获取当前最小值，如果最小值比目标值大，说明后面越来越大的值根本没戏*/
-            int min1=nums[k]+nums[k+1]+nums[k+2]+nums[k+3];
-            if(min1>target){
-                break;
-            }
-            /*获取当前最大值，如果最大值比目标值小，说明后面越来越小的值根本没戏，忽略*/
-            int max1=nums[k]+nums[length-1]+nums[length-2]+nums[length-3];
-            if(max1<target){
-                continue;
-            }
-            /*第二层循环i，初始值指向k+1*/
-            for(int i = k+1; i<length-2; i++){
-                /*当i的值与前面的值相等时忽略*/
-                if(i > k+1 && nums[i]==nums[i-1]){
-                    continue;
-                }
-                /*定义指针j指向i+1*/
-                int j=i+1;
-                /*定义指针h指向数组末尾*/
-                int h=length-1;
-                /*获取当前最小值，如果最小值比目标值大，说明后面越来越大的值根本没戏，忽略*/
-                int min=nums[k]+nums[i]+nums[j]+nums[j+1];
-                if(min>target){
-                    continue;
-                }
-                /*获取当前最大值，如果最大值比目标值小，说明后面越来越小的值根本没戏，忽略*/
-                int max=nums[k]+nums[i]+nums[h]+nums[h-1];
-                if(max<target){
-                    continue;
-                }
-                /*开始j指针和h指针的表演，计算当前和，如果等于目标值，j++并去重，h--并去重，当当前和大于目标值时h--，当当前和小于目标值时j++*/
-                while (j<h){
-                    int curr=nums[k]+nums[i]+nums[j]+nums[h];
-                    if(curr==target){
-                        result.add(Arrays.asList(nums[k],nums[i],nums[j],nums[h]));
-                        j++;
-                        while(j < h && nums[j]==nums[j-1]){
-                            j++;
+
+        for (int i = 0; i < len - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int minValueOne = nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3];
+            int maxValueOne = nums[i] + nums[len - 3] + nums[len - 2] + nums[len - 1];
+            if (minValueOne > target) continue;
+            if ( maxValueOne < target) continue;
+
+            for (int j = i + 1; j < len - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int first = j + 1;
+                int last = len - 1;
+                int minValueTwo = nums[i] + nums[j] + nums[first] + nums[first + 1];
+                int maxValueTwo = nums[i] + nums[j] + nums[last - 1] + nums[last];
+                if (minValueTwo > target) continue;
+                if ( maxValueTwo < target) continue;
+
+                while (first < last) {
+                    int sum = nums[i] + nums[j] + nums[first] + nums[last];;
+                    if (sum == target) {
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[first], nums[last]));
+                        first += 1;
+                        last -= 1;
+                        while (first < last && nums[first] == nums[first-1]) first += 1;
+                        while (first < last && nums[last] == nums[last + 1]) {
+                            last -= 1;
                         }
-                        h--;
-                        while(j < h && i<h&&nums[h]==nums[h+1]){
-                            h--;
-                        }
-                    }else if(curr>target){
-                        h--;
-                    }else {
-                        j++;
-                    }
+                    } else if (sum > target) {
+                        last -= 1;
+                    } else first += 1;
                 }
             }
         }
-        return result;
-
-
-
-
-
+        return ans;
+        
 
     }
 }
