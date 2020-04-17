2b282e2234c9b70067aebb48f8ab78f374d7556b
Yifan <yifanluo33@gmail.com>
2020-04-17 21:58:19 +0800

    Leetcode 25: Reverse nodes in k group, completed

--- editor/cn/[25]K 个一组翻转链表.java
+++ editor/cn/[25]K 个一组翻转链表.java
@@ -26,6 +26,9 @@
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.List;
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -36,47 +39,80 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        if (head == null)
+        if (head == null) {
             return null;
-        ListNode sub_head = head;
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode tail = dummy;
-        ListNode toNull = head;
-        while (sub_head != null) {
-            int i = k;
-            //找到子链表的尾部
-            while (i - 1 > 0) {
-                toNull = toNull.next;
-                if (toNull == null) {
-                    return dummy.next;
-                }
-                i--;
+        }
+
+        //五个Listnode initialization
+        ListNode sentinel = new ListNode(0);
+        sentinel.next = head;
+        ListNode tail = sentinel;
+        ListNode subHead = head;
+        ListNode isNull = head;
+
+        // while 的判定不用 head != null 的原因在于：
+        // 第一次翻转链表之后，head将会变成翻转链表的tail，
+        // 并且指向设立的空节点null，所以while将会一直为真（存疑，因为while并没有死循环）
+        while (subHead != null) {
+            // for 循环的次数是 k-2，相当于判断加上 head
+            // 及其之后的 k-1 个 node 是否为null
+            for (int i = 0; i < k - 1 ; i++) {
+                isNull = isNull.next;
+                if (isNull == null) return sentinel.next;
             }
-            ListNode temp = toNull.next;
-            //将子链表断开
-            toNull.next = null;
-            ListNode new_sub_head = reverse(sub_head);
-            //将倒置后的链表接到 tail 后边
-            tail.next = new_sub_head;
-            //更新 tail
-            tail = sub_head; //sub_head 由于倒置其实是新链表的尾部
-            sub_head = temp;
-            toNull = sub_head;
-            //将后边断开的链表接回来
-            tail.next = sub_head;
+
+            // 保存 k+1 个 node 之后的链表地址
+            ListNode tmp = isNull.next;
+            // 将 head 到之后 k 个节点的链表断开
+            isNull.next = null;
+            ListNode tmpSubHead = reverse(subHead);
+            // 此时的tail 还是指向 sentinel
+            // tmpSubHead 是翻转之后的链表，将其拼接到tail后面
+            // 也就是sentinel后面
+            tail.next = tmpSubHead;
+            // reverse 之后，subHead成了尾节点，将tail指向subHead，相当于之前
+            // 将 tail 指向sentinel，此步骤为了更新 tail 节点
+            // 注意因为翻转操作，此时的subHead已经从之前的头部变成了尾部
+            tail = subHead;
+            // isNull 指向之前保存的 k+1 节点
+            isNull = tmp;
+            // 更新 subHead，将其重新指向下一段未翻转的链表处
+            subHead = tmp;
+            tail.next = tmp;
         }
-        return dummy.next;
+        return sentinel.next;
     }
+
     public ListNode reverse(ListNode head) {
-        ListNode current_head = null;
+        ListNode prev = null;
+        ListNode cur = null;
         while (head != null) {
-            ListNode next = head.next;
-            head.next = current_head;
-            current_head = head;
-            head = next;
+            cur = head.next;
+            head.next = prev;
+            prev = head;
+            head = cur;
         }
-        return current_head;
+        return prev;
+
+//        ListNode prev = null;
+//        ListNode cur = head;
+//        while (cur != null) {
+//            ListNode tmp = cur.next;
+//            head.next = prev;
+//            prev = cur;
+//            cur = tmp;
+//        }
+//        return prev;
+
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
+
+
+
+
+
+
+
+
+
