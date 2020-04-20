20d5786299e811a45525f03daf6a455c514c9bf5
Yifan <yifanluo33@gmail.com>
2020-04-20 20:35:43 +0800

    Leetcode 82. Completed.

---
+++ editor/cn/[86]分隔链表.java
@@ -0,0 +1,66 @@
+//给定一个链表和一个特定值 x，对链表进行分隔，使得所有小于 x 的节点都在大于或等于 x 的节点之前。 
+//
+// 你应当保留两个分区中每个节点的初始相对位置。 
+//
+// 示例: 
+//
+// 输入: head = 1->4->3->2->5->2, x = 3
+//输出: 1->2->2->4->3->5
+// 
+// Related Topics 链表 双指针
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode sentinel = new ListNode(0);
+        sentinel.next = head;
+        ListNode tail = sentinel;
+        head = sentinel;
+
+        // 先找到第一个大于等于 x 的node，然后将 tail节点指向它
+        // 然后不停的往后循环，遇到val 小于 x 的node，就将其删除，然后插入到tail后面
+        while (head != null && head.next != null) {
+            if (head.next.val >= x) {
+                tail = head;
+                head = head.next;
+                break;
+            } else {
+                head = head.next;
+            }
+        }
+
+        while (head != null && head.next != null) {
+            // 节点小于x，则将其拿出，插入到tail后面
+            if (head.next.val < x) {
+                // 将要移动的node暂存在move中
+                // 将head 指向下下个节点
+                // 将暂时的move节点指向tail的下一个节点，
+                // 则 tail.next 有两个指针指向它，一个是 tail，一个是move
+                // 将tail 指向move。则插入操作完成
+                // 最后，要记得移动tail节点
+                ListNode move = head.next;
+                head.next = head.next.next;
+                move.next = tail.next;
+                tail.next = move;
+                tail = tail.next;
+//                tail = move;
+            } else {
+                head = head.next;
+            }
+        }
+        return sentinel.next;
+
+
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
