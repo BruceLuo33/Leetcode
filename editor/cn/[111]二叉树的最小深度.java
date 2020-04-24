18cb67603df9ff76dd6c7e36a71fdfefb6ef29b1
Yifan <yifanluo33@gmail.com>
2020-04-24 20:46:46 +0800

    Leetcode 111. Completed.

--- editor/cn/[111]二叉树的最小深度.java
+++ editor/cn/[111]二叉树的最小深度.java
@@ -30,7 +30,27 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
+        // 4.24 第一遍
+        // 思路：一开始的时候，想着和 104 题很相似，那是不是直接将 return 的项由 Math.max 转换成 Math.min 就可以呢？
+        // 明显不是。因为如果某一个子节点为 null，则这个方法就会选择这个子节点。但事实上这样算出来对于 spindly 的case，
+        // 也就是 worst case，会出现明显的误判。因此需要加两行判断，即如果左子节点为 null，就 return minDepth(root.left) + 1，
+        // 如果右子节点为 null，则返回左子节点。
+        // 注意：判断左右子节点为null，return 的项要 +1. 这表示循环往下走了一层。否则循环往下走，但是 height 没有记录，就会出错。
+        // 复杂度分析：O(N)
 
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        }
+
+        if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
