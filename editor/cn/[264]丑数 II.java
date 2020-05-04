d48b10dab01ceb1b494fbd1d1f5417c1ee3a811d
Yifan <yifanluo33@gmail.com>
2020-05-04 23:54:19 +0800

    Leetcode 264. Rewrite, completed.

--- editor/cn/[264]丑数 II.java
+++ editor/cn/[264]丑数 II.java
@@ -20,7 +20,7 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int nthUglyNumber(int n) {
-        // 4.25 第一遍
+        // 4.25 第一遍，4.27 第二遍，5.4第三遍
         // 思路：和263不同，这里是需要将丑数的规律找出来，类似于找到一个通项公式。
         // 通过观察可以发现，每一个丑数，都是 2x1、3x1、5x1 …… 2xN、3xN、5xN 来得出的，
         // 或者从另一个角度而言，每一个丑数 Xn，都是前面的一个某丑数（不一定是Xn-1）对应地乘 2 or 3 or 5 得到的。
@@ -31,22 +31,41 @@ class Solution {
         // 而应该都是 if
         // 复杂度分析：O（N），空间复杂度：O（k），k为input n 的最大值，1690.
 
-        int[] ans = new int[n];
-        ans[0] = 1;
-        int factor2 = 0, factor3 = 0, factor5 = 0;
-        int index2 = 0, index3 = 0, index5 = 0;
 
-        for (int i = 1; i < n; i++){
-            factor2 = 2 * ans[index2];
-            factor3 = 3 * ans[index3];
-            factor5 = 5 * ans[index5];
-            int min = Math.min(Math.min(factor2, factor3), factor5);
-            ans[i] = min;
-            if (factor2 == min) index2 += 1;
-            if (factor3 == min) index3 += 1;
-            if (factor5 == min) index5 += 1;
+
+        int[] ans = new int[1690];
+        ans[0] = 1;
+        int product2 = 0, product3 = 0, product5 = 0;
+        int index2 = 0, index3 = 0, index5 = 0;
+        for (int i = 1; i < n; i++) {
+            product2 = 2 * ans[index2];
+            product3 = 3 * ans[index3];
+            product5 = 5 * ans[index5];
+            ans[i] = Math.min(Math.min(product2, product3), product5);
+            if (ans[i] == product2) index2 += 1;
+            if (ans[i] == product3) index3 += 1;
+            if (ans[i] == product5) index5 += 1;
         }
         return ans[n-1];
+
+
+
+        // int[] ans = new int[n];
+        // ans[0] = 1;
+        // int factor2 = 0, factor3 = 0, factor5 = 0;
+        // int index2 = 0, index3 = 0, index5 = 0;
+
+        // for (int i = 1; i < n; i++){
+        //     factor2 = 2 * ans[index2];
+        //     factor3 = 3 * ans[index3];
+        //     factor5 = 5 * ans[index5];
+        //     int min = Math.min(Math.min(factor2, factor3), factor5);
+        //     ans[i] = min;
+        //     if (factor2 == min) index2 += 1;
+        //     if (factor3 == min) index3 += 1;
+        //     if (factor5 == min) index5 += 1;
+        // }
+        // return ans[n-1];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
