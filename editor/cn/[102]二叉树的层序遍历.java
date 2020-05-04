098a8fe613bd0e70ff1b1dfabd3bd1f34a72fc3b
Yifan <yifanluo33@gmail.com>
2020-05-04 23:50:51 +0800

    Leetcode 102. Rewrite, completed.

--- editor/cn/[102]二叉树的层序遍历.java
+++ editor/cn/[102]二叉树的层序遍历.java
@@ -35,36 +35,92 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        // 4.28 第一遍
+        // 4.28 第一遍，4.29 第二遍，5.4 第三遍
         // 思路：使用 BFS 对树进行层间遍历。每次都用一个队列来接受root，然后将其放入答案链表即可。
-        // 复杂度分析：O（）
+        // 注意：在判定当前子节点等级与 ans size 的关系的时候，要写 >= 而非 >，因为在一开始的时候，二者都是 0，如果不加 =，整个
+        // 循环就无法开始。
+        // 复杂度分析：O（N） 空间复杂度：O（N）
 
         List<List<Integer>> ans = new ArrayList<>();
-        if (root == null) return ans;
-
-        int level = 0;
-        Queue<TreeNode> treeNode = new LinkedList<>();
+        Queue<TreeNode> node = new LinkedList<>();
         Queue<Integer> nodeLevel = new LinkedList<>();
-        treeNode.offer(root);
+        node.offer(root);
+        int level = 0;
         nodeLevel.offer(level);
 
-        while(!treeNode.isEmpty()) {
-            TreeNode curNode = treeNode.poll();
-            int curLevel = nodeLevel.poll();
-            if(curNode != null) {
-                if (ans.size() <= curLevel) {
+        while (!node.isEmpty()) {
+            TreeNode curNode = node.poll();
+            Integer curLevel = nodeLevel.poll();
+            if (curNode != null) {
+                if (curLevel >= ans.size()) {
                     ans.add(new ArrayList<>());
                 }
-
                 ans.get(curLevel).add(curNode.val);
                 level = curLevel + 1;
-                treeNode.offer(curNode.left);
-                treeNode.offer(curNode.right);
+                node.offer(curNode.left);
+                node.offer(curNode.right);
                 nodeLevel.offer(level);
                 nodeLevel.offer(level);
             }
+
         }
         return ans;
+
+
+
+        // List<List<Integer>> ans = new ArrayList<>();
+        // if (root == null) return ans;
+
+        // Queue<TreeNode> node = new LinkedList<>();
+        // Queue<Integer> nodeLevel = new LinkedList<>();
+        // int level = 0;
+        // node.offer(root);
+        // nodeLevel.offer(level);
+
+        // while(!node.isEmpty()) {
+        //     TreeNode curNode = node.poll();
+        //     int curLevel = nodeLevel.poll();
+        //     if (curNode != null) {
+        //         if (curLevel >= ans.size()) {
+        //             ans.add(new ArrayList<>());
+        //         }
+        //         ans.get(curLevel).add(curNode.val);
+        //         level = curLevel + 1;
+        //         node.offer(curNode.left);
+        //         node.offer(curNode.right);
+        //         nodeLevel.offer(level);
+        //         nodeLevel.offer(level);
+        //     }
+        // }
+        // return ans;
+
+
+        // List<List<Integer>> ans = new ArrayList<>();
+        // if (root == null) return ans;
+
+        // int level = 0;
+        // Queue<TreeNode> treeNode = new LinkedList<>();
+        // Queue<Integer> nodeLevel = new LinkedList<>();
+        // treeNode.offer(root);
+        // nodeLevel.offer(level);
+
+        // while(!treeNode.isEmpty()) {
+        //     TreeNode curNode = treeNode.poll();
+        //     int curLevel = nodeLevel.poll();
+        //     if(curNode != null) {
+        //         if (ans.size() <= curLevel) {
+        //             ans.add(new ArrayList<>());
+        //         }
+
+        //         ans.get(curLevel).add(curNode.val);
+        //         level = curLevel + 1;
+        //         treeNode.offer(curNode.left);
+        //         treeNode.offer(curNode.right);
+        //         nodeLevel.offer(level);
+        //         nodeLevel.offer(level);
+        //     }
+        // }
+        // return ans;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
