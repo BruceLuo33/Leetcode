4cf61dffd956ae3b380b0d78b1a0eb95a5769c89
Yifan <yifanluo33@gmail.com>
2020-05-05 23:27:35 +0800

    Leetcode 109. Rewrite, completed.

--- editor/cn/[109]有序链表转换二叉搜索树.java
+++ editor/cn/[109]有序链表转换二叉搜索树.java
@@ -36,7 +36,7 @@
  * }
  */
 class Solution {
-    // 4.30 第一遍
+    // 4.30 第一遍，5.5 第二遍
     // 分析：这道题和 108 题非常相似，区别在于链表不支持随机访问，所以获取根节点的坐标会相对困难。在此我们可以提出三种解法思路。
     // 思路一：既然链表无法随机访问，我改成一个可以随机访问的不就行了吗？于是可以新建一个 `ArrayList<Integer>` 用来接收给定
     // List 中的元素，其他的步骤和 108 题一模一样，时间/空间复杂度为 O（N）
@@ -46,34 +46,36 @@ class Solution {
     // 思路三：参考了答案，得到了一种最佳的解法。这种做法是模仿中序遍历。在中序遍历中，我们采用的方式是将一个二叉树按顺序赋值给一个数组，在这里，我们可以采用相似的办法，但是不同的点在于这一次是利用中序遍历赋值给二叉树。
     // 注意：要设置一个全局变量的指针指向head，设置全局变量的原因是递归加入指针的参数会很麻烦
     // 注意：在遍历右子树之后，cur不再需要后移，
-    ListNode curNode = null;
 
-    // Solution 3 codes：
-    public TreeNode sortedListToBST(ListNode head) {
-        int end = 0;
-        curNode = head;
-        while (head != null) {
-            end += 1;
-            head = head.next;
-        }
-        return helperThree(0, end);
-    }
 
-    private TreeNode helperThree(int start, int end) {
-        if (start == end) return null;
-        int mid = (int) (start + end) / 2;
+    // // Solution 3 codes：
+    // ListNode curNode = null;
+    // public TreeNode sortedListToBST(ListNode head) {
+    //     int end = 0;
+    //     curNode = head;
+    //     while (head != null) {
+    //         end += 1;
+    //         head = head.next;
+    //     }
+    //     return helperThree(0, end);
+    // }
 
-        // 遍历左子树，找到左子根节点，此时 start == null，会返回 null
-        TreeNode left = helperThree(start, mid);
-        TreeNode root = new TreeNode(curNode.val);
-        root.left = left;
-        curNode = curNode.next;
-        // 遍历右子树，找到右子根节点，此时 start == null，会返回 null
-        TreeNode right = helperThree(mid + 1, end);
-        root.right = right;
+    // private TreeNode helperThree(int start, int end) {
+    //     if (start == end) return null;
+    //     int mid = (int) (start + end) / 2;
 
-        return root;
-    }
+    //     // 遍历左子树，找到左子根节点，此时 start == null，会返回 null
+    //     TreeNode left = helperThree(start, mid);
+    //     TreeNode root = new TreeNode(curNode.val);
+    //     root.left = left;
+
+    //     curNode = curNode.next;
+    //     // 遍历右子树，找到右子根节点，此时 start == null，会返回 null
+    //     TreeNode right = helperThree(mid + 1, end);
+    //     root.right = right;
+
+    //     return root;
+    // }
 
 
 
@@ -95,5 +97,30 @@ class Solution {
     //     root.right = helper(listValue, mid + 1, end);
     //     return root;
     // }
+
+
+
+
+    public TreeNode sortedListToBST(ListNode head) {
+        ArrayList<Integer> restore = new ArrayList<>();
+        int count = 0;
+        while (head != null) {
+            restore.add(head.val);
+            count += 1;
+            head = head.next;
+        }
+        return helper(restore, 0, count);
+    }
+
+    private TreeNode helper(ArrayList<Integer> restore, int start, int end) {
+        if (start == end) return null;
+        int mid = (int) (start + end) / 2;
+        int rootVal = restore.get(mid);
+        TreeNode root = new TreeNode(rootVal);
+        root.left = helper(restore, start, mid);
+        root.right = helper(restore, mid + 1, end);
+        return root;
+    }
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
