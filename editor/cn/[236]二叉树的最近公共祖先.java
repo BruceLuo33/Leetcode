2ac71ab9fe8d5a3583543a391a8f694c39dd5e15
Yifan <yifanluo33@gmail.com>
2020-05-15 10:14:59 +0800

    Leetcode 236. Rewrite.

--- editor/cn/[236]二叉树的最近公共祖先.java
+++ editor/cn/[236]二叉树的最近公共祖先.java
@@ -44,16 +44,17 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+/**
+ 5.10 第一遍，5.11 第二遍，5.15 第三遍
+ 思路：递归。关键问题在于对参数 `left` 和 `right` 的理解，因为这是递归，所以在写递归语句的时候，可以认为左右子树都已经算出来了结果。
+ 1. 如果当前节点 root 为 null，那么就直接返回 null；
+ 2. 如果 root 等于 p 或者是 q，那么就返回 p or q；
+ 3. 接着往下递归。如上所述，递归的时候可以直接认为我们已经求出来了 left 和 right，用它们来表示即可；
+ 4. 对于返回值，有以下几种情况：第一，left 和 right 同时为空，说明左右子树都不包含p、q，返回null；第二，同时不为空，说明在root 的两侧，返回root；第三，left 为空，right不为空，说明p、q都在右边；第四，right 为空，left不为空，说明都在左边
+ 复杂度分析：O（N）
+ */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // 5.10 第一遍，5.11 第二遍
-        // 思路：递归。关键问题在于对参数 `left` 和 `right` 的理解，因为这是递归，所以在写递归语句的时候，可以认为左右子树都已经算出来了结果。
-        // 1. 如果当前节点 root 为 null，那么就直接返回 null；
-        // 2. 如果 root 等于 p 或者是 q，那么就返回 p or q；
-        // 3. 接着往下递归。如上所述，递归的时候可以直接认为我们已经求出来了 left 和 right，用它们来表示即可；
-        // 4. 对于返回值，有以下几种情况：第一，left 和 right 同时为空，说明左右子树都不包含p、q，返回null；第二，同时不为空，说明在root 的两侧，返回root；第三，left 为空，right不为空，说明p、q都在右边；第四，right 为空，left不为空，说明都在左边
-        // 复杂度分析：O（N）
-
 
 
         if (root == null || root == q || root == p) return root;
