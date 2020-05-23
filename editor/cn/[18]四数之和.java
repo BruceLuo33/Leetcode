05b8630c66f175c69311fb8bac27dbd025847c2c
Yifan <yifanluo33@gmail.com>
2020-05-23 12:05:30 +0800

    Leetcode 18. Rewrite.

--- editor/cn/[18]四数之和.java
+++ editor/cn/[18]四数之和.java
@@ -23,6 +23,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ 4.12 第一遍，5.23 第二遍
+ - 思路：和三数之和很类似，关键在于剪枝：
+ 1. 最开始的时候要判断数组元素是否大于四个；
+ 2. 每一个 for loop 都要判断是否出现了相同的元素，如果是，就continue；
+ 3. 第一个循环的时候，要判断 `min > target` 和 `max < target` 是否成立，注意如果成立，也不能代表可以直接 return ans，要 continue；
+ 4. 最内存的 while 循环，当 sum = target 的时候，要注意除了 first 与 last 的移动之外，还需要剪枝，即判断是否有相同元素出现，如果是，则跳过
+ 复杂度分析：O（N^3）
+ */
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> ans = new ArrayList<>();
@@ -64,8 +73,6 @@ class Solution {
             }
         }
         return ans;
-
-
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
