8f3844f89484605d69b03d330f6fa2488edcd6f3
Yifan <yifanluo33@gmail.com>
2020-05-29 22:18:57 +0800

    Leetcode 121. Completed.

--- editor/cn/[121]买卖股票的最佳时机.java
+++ editor/cn/[121]买卖股票的最佳时机.java
@@ -40,6 +40,7 @@
  - 复杂度分析：O（N），空间复杂度：O（N），可以提升为 O（1）
  */
 
+
 class Solution {
     public int maxProfit(int[] prices) {
         if (prices.length == 0) return 0;
@@ -57,8 +58,19 @@ class Solution {
             dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
             dp[i][1] = Math.max(dp[i-1][1],  - prices[i]);
         }
-        // return Math.max(dp[n-1][0], dp[n-1][1]);
-        return dp[n-1][0];
+        return Math.max(dp[n-1][0], dp[n-1][1]);
+    }
+
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0) return 0;
+        int n = prices.length, k = 1;
+        int dp_i0 = 0, dp_i1 = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            dp_i0 = Math.max(dp_i0, dp_i1 + prices[i]);
+            dp_i1 = Math.max(dp_i1, -prices[i]);
+        }
+        return dp_i0;
     }
 }
+
 //leetcode submit region end(Prohibit modification and deletion)
