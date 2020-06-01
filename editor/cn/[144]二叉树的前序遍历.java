70b5db139d21609150cd28064287dfbac817b770
Yifan <yifanluo33@gmail.com>
2020-06-01 23:14:28 +0800

    Leetcode 144. Rewrite.

--- editor/cn/[144]二叉树的前序遍历.java
+++ editor/cn/[144]二叉树的前序遍历.java
@@ -26,40 +26,54 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+/**
+ 4.24 第一遍，5.4 第二遍
+ - 思路一：前序遍历，经典算法。
+ 1. append root value
+ 2. 递归左树
+ 3. 递归右树
+ - 复杂度分析：O（N），空间复杂度：O（logN）~ O（N），取决于 root 的形态
+ - 思路二：与 94 题一样，用 stack 来辅助进行递归，步骤如下：
+ 1. 把根结点加入stack中。
+ 2. 开始遍历 while(!stack.isEmpty()) 执行下面的3、4步
+ 3. 从stack中取出栈顶的TreeNode node结点，把它加入到结果集res中
+ 4. 依次加入node的右孩子、左孩子（如果存在的话）
+ 5. 得到结果集res，返回
+ */
 class Solution {
-    // 4.24 第一遍，5.4 第二遍
-    // 思路：前序遍历，经典算法。
-    // 第一步：append root value
-    // 第二步：递归左树
-    // 第三步：递归右树
-    // 复杂度分析：O（N），空间复杂度：O（logN）~ O（N），取决于 root 的形态
 
+    // Solution Two：Iteration
     public List<Integer> preorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
         List<Integer> ans = new ArrayList<>();
-        helper(root, ans);
+        if (root == null) return ans;
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            ans.add(cur.val);
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+        }
         return ans;
     }
 
-    private void helper(TreeNode root, List<Integer> ans) {
-        if (root == null) return;
 
-        ans.add(root.val);
-        helper(root.left, ans);
-        helper(root.right, ans);
-    }
-
-
+    // Solution One：Recursive
     // public List<Integer> preorderTraversal(TreeNode root) {
-    //   List<Integer> ans = new ArrayList<>();
-    //     preOrderHelper(root, ans);
+    //     List<Integer> ans = new ArrayList<>();
+    //     helper(root, ans);
     //     return ans;
     // }
-
-    // public void preOrderHelper(TreeNode root, List<Integer> ans) {
+    // private void helper(TreeNode root, List<Integer> ans) {
     //     if (root == null) return;
+
     //     ans.add(root.val);
-    //     preOrderHelper(root.left, ans);
-    //     preOrderHelper(root.right, ans);
+    //     helper(root.left, ans);
+    //     helper(root.right, ans);
     // }
 }
 //leetcode submit region end(Prohibit modification and deletion)
