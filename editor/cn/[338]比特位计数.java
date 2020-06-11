31c5f2d48ab865de5dfbb5373ed0934aba961ca6
Yifan <yifanluo33@gmail.com>
2020-06-11 23:58:23 +0800

    Leetcode 338. Completed.

--- editor/cn/[338]比特位计数.java
+++ editor/cn/[338]比特位计数.java
@@ -21,9 +21,30 @@
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ 6.11 第一遍
+ - 思路：位运算 + 动态规划。
+ 1. 方法一，i&(i-1) 可以清零最低位的 1，最低位不一定就是从右到左第一位。而这个数字是一定比 i 小的，也就是说一定是出现在 ans 数组里面过的，然后因为减去了一个 1，再将 ans 数组 +1 就可以了；
+ 2. 方法二：i >> 1 代表的是将 i/2，如果 i 是偶数，那么 ans[i] == ans[i >> 1]，而如果是 i 是奇数，则 ans[i] = ans[i >> 1] + 1，代表比 i/2 多了一个最低位的 1，因为是否 +1 和奇偶有关，所以用 i&1 来表示。
+ */
 class Solution {
+    /**
+     // Solution one:
+     public int[] countBits(int num) {
+     int[] ans = new int[num + 1];
+     for (int i = 1; i <= num; i++) {
+     ans[i] = ans[i & (i-1)] + 1;
+     }
+     return ans;
+     }
+     */
+    // Solution two:
     public int[] countBits(int num) {
-
+        int[] ans = new int[num + 1];
+        for (int i = 0; i <= num; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+        return ans;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
