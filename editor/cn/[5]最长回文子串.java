cc8cd73e419abec1b53b3d6d460158ce2c723e59
Yifan <yifanluo33@gmail.com>
2020-06-21 23:19:05 +0800

    Leetcode 5. Rewrited.

--- editor/cn/[5]最长回文子串.java
+++ editor/cn/[5]最长回文子串.java
@@ -17,10 +17,15 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 /**
- 6.17 第一遍
- - 思路：动态规划。
+ 6.17 第一遍，6.21 第二遍
+ - 思路一：动态规划。
  1. 首先弄清楚题目需要的是什么：“子串”，代表的是一个连续的字符串，而不是“子序列”。后者并不要求连续；
- 2. 对于子串问题，应该设置 dp 为二维矩阵；
+ 2. 对于子串问题，应该设置 dp 为二维 boolean 矩阵，和 1143、516 类似，dp[i][j] 表示的意思是子串 s[i..j]是否为回文子串，如果 `dp[i][j] == true`，说明是，否则不是；
+ 3. 对于状态转移方程，考虑这种情况：如果子串只有一个或者是两个字符，那么我们很容易判断 true or false，这也就是 base case；
+ 4. 而对于更加 general 的情况，就需要用到前面的 dp 数组了，因为此时不会再产生数组越界问题，所以直接 `dp[i][j] = dp[i + 1][j - 1]` 即可;
+ 5. 对于这种二维数组的情况，需要考虑 `dp[i][j]` 需要预先知道哪个方位的数据，从上面可以看出，我们需要先知道 `dp[i+1][j-1]` 的值，也就是左下角，因此两层 for 循环的终点应该是右上角
+ - 复杂度分析：O(N^2)，空间复杂度：O(N^2)
+ - 思路二：
 
  */
 class Solution {
@@ -29,18 +34,14 @@ class Solution {
         if (m < 2) return s;
         boolean[][] dp = new boolean[m][m];
         char[] sChar = s.toCharArray();
-        int maxLen = 1, begin = 0;
+        int begin = 0, maxLen = 0;
         for (int i = 0; i < m; i++) dp[i][i] = true;
-        for (int j = 1; j < m ; j++) {
-            for (int i = 0; i < j; i++) {
-                if (sChar[i] != sChar[j]) {
-                    dp[i][j] = false;
-                } else {
-                    if (j - i < 3) {
-                        dp[i][j] = true;
-                    } else {
-                        dp[i][j] = dp[i + 1][j - 1];
-                    }
+        for (int i = m - 1; i >=0; i--) {
+            for (int j = i; j < m; j++) {
+                if (sChar[i] != sChar[j]) dp[i][j] = false;
+                else  {
+                    if (j - i + 1 < 3) dp[i][j] = true;
+                    else dp[i][j] = dp[i + 1][j - 1];
                 }
                 if (dp[i][j] && j - i + 1 > maxLen) {
                     maxLen = j - i + 1;
