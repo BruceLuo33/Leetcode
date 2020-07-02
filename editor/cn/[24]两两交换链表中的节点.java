9b73e11fcd7e4e1daca79d5d93b3e79c7ff0e867
Yifan <yifanluo33@gmail.com>
2020-07-02 09:15:19 +0800

    Leetcode 24. Rewrited.

--- editor/cn/[24]两两交换链表中的节点.java
+++ editor/cn/[24]两两交换链表中的节点.java
@@ -20,84 +20,39 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+/**
+ 4.16 第一遍，4.22 第二遍，5.6 第三遍，7.2 第四遍
+ - 思路：
+ 1. 如果是空节点 or 单节点，直接返回 head;
+ 2. 设置 sentinel 节点指向初始的 head，方便最终结果返回;
+ 3. 设置 nextMove 节点，保证 nextMove.next 一直指向 head;
+ 4. 设置 tmp 保存反转节点之后的 node，即 head.next.next;
+ - 注意：在循环的最后，要记得将 head 也指向tmp，亦即之前的 head.next.next，否则会出现以下情况
+
+ ```Java
+ Original: 1 -> 2 -> 3 -> 4
+ First swap: 2 -> 1 -> 3 -> 4
+ Second swap: 2 -> 3 -> 1 -> 4 (×)
+ 2 -> 1 -> 4 -> 3 (√)
+ ```
+ */
 class Solution {
     public ListNode swapPairs(ListNode head) {
 
-        // 4.16 第一遍，4.22 第二遍，5.6 第三遍
-        // 思路：
-        // 1. 如果是空节点 or 单节点，直接返回 head
-        // 2. 设置 sentinel 节点指向初始的 head，方便最终结果返回
-        // 3. 设置 node 节点对
-
-        // 5.6 codes
         if (head == null || head.next == null) return head;
         ListNode sentinel = new ListNode(0);
         sentinel.next = head;
-        ListNode nextMove = sentinel;
+        ListNode nextNodeMove = sentinel;
         while (head != null && head.next != null) {
-            // ListNode tmp = head.next;
             ListNode tmp = head.next.next;
-            nextMove.next = head.next;
+            nextNodeMove.next = head.next;
             head.next.next = head;
             head.next = tmp;
-
-        }
-        return sentinel.next;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // 4.22 codes
-        if (head == null || head.next == null) return head;
-        ListNode sentinel = new ListNode(0);
-        sentinel.next = head;
-        ListNode move = sentinel;
-        while (head != null && head.next!= null) {
-            ListNode tmp = head.next.next;
-            move.next = head.next;
-            head.next.next = head;
-            head.next = tmp;
-            move = head;
+            nextNodeMove = head;
             head = tmp;
         }
         return sentinel.next;
 
-        // 4.16 codes
-        ListNode sentinel = new ListNode(0);
-        sentinel.next = head;
-        ListNode cur = sentinel;
-        while (cur.next != null && cur.next.next != null) {
-//            ListNode nodeOne = head;
-            ListNode nodeOne = cur.next;
-            ListNode nodeTwo = cur.next.next;
-            cur.next = nodeTwo;
-            nodeOne.next = nodeTwo.next;
-            nodeTwo.next = nodeOne;
-            cur = nodeOne;
-        }
-        return sentinel.next;
     }
-
-
-
 }
 //leetcode submit region end(Prohibit modification and deletion)
