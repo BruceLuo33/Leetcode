740781f0214130d8a4fdd5575d3075fecb82cda5
Yifan <yifanluo33@gmail.com>
2020-07-22 13:37:01 +0800

    Leetcode 347. Completed. 2020/07/22.

---
+++ editor/cn/[347]前 K 个高频元素.java
@@ -0,0 +1,71 @@
+//给定一个非空的整数数组，返回其中出现频率前 k 高的元素。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: nums = [1,1,1,2,2,3], k = 2
+//输出: [1,2]
+// 
+//
+// 示例 2: 
+//
+// 输入: nums = [1], k = 1
+//输出: [1] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 你可以假设给定的 k 总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。 
+// 你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。 
+// 题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的。 
+// 你可以按任意顺序返回答案。 
+// 
+// Related Topics 堆 哈希表
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ 7.22 第一遍
+ - 思路：优先队列/最小堆
+
+ */
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        int[] ans = new int[k];
+        if (nums == null || nums.length == 0) return ans;
+        // 用 HashMap 来统计频数
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            } else {
+                map.put(i, 1);
+            }
+        }
+        // 用优先队列来创建小顶堆
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>(){
+            @Override
+            public int compare(Integer a, Integer b) {
+                return map.get(a) - map.get(b);
+            }
+        });
+        for (Integer key : map.keySet()) {
+            if (pq.size() < k) {
+                pq.add(key);
+            } else if (map.get(key) > map.get(pq.peek())) {
+                pq.remove();
+                pq.add(key);
+            }
+        }
+        // 取出最小堆中的元素
+        int count = k - 1;
+        while (!pq.isEmpty()) {
+            ans[count--] = pq.remove();
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
