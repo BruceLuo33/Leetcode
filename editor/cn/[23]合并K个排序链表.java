19a7cfcc1dbe64fbd05797798900ded3306de5bc
Yifan <yifanluo33@gmail.com>
2020-07-24 12:09:15 +0800

    Leetcode 23. Completed.

--- editor/cn/[23]合并K个排序链表.java
+++ editor/cn/[23]合并K个排序链表.java
@@ -21,9 +21,60 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+/**
+ 7.24 第一遍
+ - 思路一：小顶堆
+ - 思路二：分治
+ */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-
+        if (lists == null || lists.length == 0) return null;
+        return helper(0, lists.length - 1, lists);
     }
+    private ListNode helper(int begin, int end, ListNode[] lists) {
+        if (begin == end) return lists[begin];
+        int mid = begin + (end - begin) / 2;
+        ListNode left = helper(begin, mid, lists);
+        ListNode right = helper(mid + 1, end, lists);
+        return merge(left, right);
+    }
+    private ListNode merge(ListNode a, ListNode b) {
+        if (a == null || b == null) {
+            return a == null ? b : a;
+        } else if (a.val < b.val) {
+            a.next = merge(a.next, b);
+            return a;
+        } else {
+            b.next = merge(a, b.next);
+            return b;
+        }
+    }
+
+// Solution One：
+    // public ListNode mergeKLists(ListNode[] lists) {
+    // if (lists == null || lists.length == 0) return null;
+    // PriorityQueue<ListNode> pq = new PriorityQueue<>(new Comparator<ListNode>() {
+    //     @Override
+    //     public int compare(ListNode one, ListNode two) {
+    //         return one.val - two.val;
+    //     }
+    // });
+
+    // for (ListNode node : lists) {
+    //     while (node != null) {
+    //         pq.add(node);
+    //         node = node.next;
+    //     }
+    // }
+    // ListNode sentinel = new ListNode(0);
+    // ListNode head = sentinel;
+    // while(!pq.isEmpty()) {
+    //     head.next = pq.remove();
+    //     head = head.next;
+    // }
+    // // 这一步很重要，为了防止形成环
+    // head.next = null;
+    // return sentinel.next;
+    // }
 }
 //leetcode submit region end(Prohibit modification and deletion)
