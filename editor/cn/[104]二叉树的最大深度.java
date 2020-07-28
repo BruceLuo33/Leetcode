d8b1f395d6f17b406dd99c56888c5cd796fe38f6
Yifan <yifanluo33@gmail.com>
2020-07-28 08:39:30 +0800

    Leetcode 104. Rewrited..

--- editor/cn/[104]二叉树的最大深度.java
+++ editor/cn/[104]二叉树的最大深度.java
@@ -29,17 +29,33 @@
  */
 /**
 4.24 第一遍，7.28 第二遍
-思路：递归。从 root 出发，最大的深度等于 1+ 左/右 的node 最大深度。
+思路一：递归。从 root 出发，最大的深度等于 1+ 左/右 的node 最大深度。
+思路二：BFS
 复杂度分析：O（N），空间复杂度：O（1）
 */
 class Solution {
     public int maxDepth(TreeNode root) {
-
-
-        // Basic condition
+        // Solution One
         public int maxDepth(TreeNode root) {
             if (root == null) return 0;
             return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        }
+        // Solution Two
+        public int maxDepth(TreeNode root) {
+            if (root == null) return 0;
+            int depth = 0;
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                depth += 1;
+                for (int i = 0; i < size; i++) {
+                    TreeNode tmpNode = queue.poll();
+                    if (tmpNode.left != null) queue.offer(tmpNode.left);
+                    if (tmpNode.right != null) queue.offer(tmpNode.right);
+                }
+            }
+            return depth;
         }
 
     }
