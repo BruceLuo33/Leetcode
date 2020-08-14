16baae86724a0c2d684e92ab02f0e1acd1b0df56
Yifan <yifanluo33@gmail.com>
2020-08-14 14:34:13 +0800

    Leetcode 20. 有效的括号. Rewrited.

--- editor/cn/[20]有效的括号.java
+++ editor/cn/[20]有效的括号.java
@@ -42,7 +42,7 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 /**
- 6.28 第一遍
+ 6.28 第一遍，8.14 第二遍
  - 思路：栈。题目的要求并不仅仅是左括号的数量等于右括号，而且还要中间没有其他括号：
  1. 遇到左括号，就将其对应的右括号压入栈；
  2. 如果不是左括号，就判断是否等于栈顶元素，如果不是，说明两个对应的括号中间有其他左括号，return false；
