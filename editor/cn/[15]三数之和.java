ee554224bac761c8147180445a416b619098c621
BruceLuo33 <yifanluo33@gmail.com>
2020-10-22 10:44:06 +0800

    Leetcode 15， 3sum 第三遍完成。三指针

--- editor/cn/[15]三数之和.java
+++ editor/cn/[15]三数之和.java
@@ -24,47 +24,48 @@ import java.util.Arrays;
 import java.util.List;
 
 //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ 4.11 第一遍，4.22 第二遍，10.22 第三遍
+ 思路：双指针。遍历整个数组。如果 3sum > target，则将最右端的指针内移，否则左移。
+ 因为需要判断指针移动方向，即 3sum 与 target 的相对大小。所以需要对数组先sort
+ 注意：因为复杂度比较高，需要多用去重。首先可以判断 min/max 和 0 的关系，如果都取不到，
+ 复杂度分析：O（N^2 + NlogN），空间复杂度 O（1）
+
+ */
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // 4.11 第一遍，4.22 第二遍
-        // 思路：双指针。遍历整个数组。如果 3sum > target，则将最右端的指针内移，否则左移。
-        // 因为需要判断指针移动方向，即 3sum 与 target 的相对大小。所以需要对数组先sort
-        // 注意：因为复杂度比较高，需要多用去重。首先可以判断 min/max 和 0 的关系，如果都取不到，则返回空数组
-        // 注意： 在for循环中，除了判断second 和 third 是否重复的情况，还要判断first 是否重复，如果是，就要 continue。
-        // 复杂度分析：O（N^2 + NlogN），空间复杂度 O（1）
 
         // 4.22 codes:
+
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
-        if (nums.length <= 2 || nums == null) return ans;
-        int first, second, third = nums.length - 1, sum;
+        int len = nums.length;
+        if (len < 3) return ans;
+        int first, second, third = len - 1, sum;
         int maxValue = nums[third] + nums[third - 1] + nums[third - 2];
         int minValue = nums[0] + nums[1] + nums[2];
-        if (maxValue < 0) return ans;
-        if (minValue > 0) return ans;
+        if (minValue > 0 || maxValue < 0) return ans;
 
-        for (first = 0; first < nums.length - 2; first++) {
-            second = first + 1;
-            third = nums.length - 1;
+        for (first = 0; first < len - 2; first++) {
             if (first > 0 && nums[first] == nums[first - 1]) {
                 continue;
             }
-            while (second < third) {
+            second = first + 1;
+            third = len - 1;
+            while(second < third) {
                 sum = nums[first] + nums[second] + nums[third];
                 if (sum == 0) {
                     ans.add(Arrays.asList(nums[first], nums[second], nums[third]));
                     second += 1;
                     third -= 1;
-                    while (second < third && nums[second] == nums[second - 1]) second += 1;
-                    while (second < third && nums[third] == nums[third + 1]) third -= 1;
-                }
-                else if (sum > 0) {
-                    third -= 1;
-                    // while (second < third && nums[third] == nums[third + 1]) third -= 1;
-                }
-                else {
+                    while(second < third && nums[second] == nums[second - 1]) second += 1;
+                    while(second < third && nums[third] == nums[third + 1]) third -= 1;
+                } else if (sum < 0) {
                     second += 1;
-                    // while (second < third && nums[second] == nums[second - 1]) second += 1;
+                    // while(second < third && nums[second] == nums[second - 1]) second += 1;
+                } else if (sum > 0) {
+                    third -= 1;
+                    // while(second < third && nums[third] == nums[third + 1]) third -= 1;
                 }
             }
         }
